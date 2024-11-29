2053f10ebac18653acbf99de7edaa978e1b49c1b
Tech Test <jkempster34@gmail.com>
2024-11-29 08:00:52 +0000

    Add request and response models
    In order for the service layer to be responsible for validation and mapping,
    these live there.

---
+++ UKParliament.CodeTest.Services/RequestResponseModels/DepartmentResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace UKParliament.CodeTest.Services.RequestResponseModels
+{
+    public class DepartmentResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ UKParliament.CodeTest.Services/RequestResponseModels/PersonResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace UKParliament.CodeTest.Services.RequestResponseModels;
+
+public class PersonResponse
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public int DepartmentId { get; set; }
+}
