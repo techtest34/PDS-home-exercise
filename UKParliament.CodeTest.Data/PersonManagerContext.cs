4cf3a77e9626e69f35ddd0b8fb83cd877ba9307f
Tech Test <jkempster34@gmail.com>
2024-11-29 08:00:40 +0000

    Seed a user

--- UKParliament.CodeTest.Data/PersonManagerContext.cs
+++ UKParliament.CodeTest.Data/PersonManagerContext.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using UKParliament.CodeTest.Data.Entities;
 
 namespace UKParliament.CodeTest.Data;
@@ -13,6 +13,16 @@ public class PersonManagerContext : DbContext, IPersonManagerContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Person>().HasData(
+            new Person
+            { 
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                DateOfBirth = new DateOnly(2000, 12, 25),
+                DepartmentId = 1 
+            });
 
         modelBuilder.Entity<Department>().HasData(
             new Department { Id = 1, Name = "Sales" },
